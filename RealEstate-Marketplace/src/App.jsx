09160abcadd0295f090ffb59490807f724ef3a3a
AlokMahapatra26 <alokmahapatra2604@gmail.com>
2023-08-27 13:14:33 +0530

    created routes

---
+++ RealEstate-Marketplace/src/App.jsx
@@ -0,0 +1,27 @@
+import {BrowserRouter as Router , Routes , Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import Offers from './pages/Offers'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import ForgotPassword from './pages/ForgotPassword'
+
+function App() {
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/profile' element={<Profile/>}></Route>
+          <Route path='/sign-in' element={<Signin/>}></Route>
+          <Route path='/sign-up' element={<Signup/>}></Route>
+          <Route path='/forgot-password' element={<ForgotPassword/>}></Route>
+          <Route path='/offers' element={<Offers/>}></Route>
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
