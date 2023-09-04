22b25333393f22ba9d9b875af35d3c8d473a0502
AlokMahapatra26 <alokmahapatra2604@gmail.com>
2023-09-04 08:37:45 +0530

    signin functionality

--- RealEstate-Marketplace/src/pages/Signin.jsx
+++ RealEstate-Marketplace/src/pages/Signin.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useState } from 'react'
 import { useNavigate } from 'react-router';
 import OAuth from '../components/OAuth';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import {toast} from 'react-toastify';
 
 export default function Signin() {
 
@@ -25,22 +27,27 @@ export default function Signin() {
     }))
    }
 
-   
+  //FIREBASE_AUTH
+   async function onSubmit(e){
+    e.preventDefault();
 
-
-
-
-
-
-
-
-
-
+    try{
+      const auth = getAuth()
+      const userCredential = await signInWithEmailAndPassword(auth , email , password)
+      if(userCredential.user){
+        navigate("/")
+      }
+      toast.success("Signin Successful");
+    }
+    catch(error){
+      toast.error("Something went wrong")
+    }
+   }
 
   return (
     <section className='flex-1 h-[calc(100vh-3rem)]  flex justify-center items-center' >
    
-   <form className='bg-white flex flex-col items-center justify-center border p-4 rounded'>
+   <form onSubmit={onSubmit} className='bg-white flex flex-col items-center justify-center border p-4 rounded'>
     <h1 className='text-2xl m-2'>Sign in</h1>
 
     <input type="text" placeholder='email' id='email' value={email} className='p-2 my-4 border rounded' onChange={onChange}/>
@@ -55,7 +62,7 @@ export default function Signin() {
         navigate('/sign-up')
       }}>Sign up</p>
     </div>
-    <button className='my-2 bg-red-400 hover:bg-red-500 transition w-full p-2 rounded text-white '>Login</button>
+    <button type="submit"  className='my-2 bg-red-400 hover:bg-red-500 transition w-full p-2 rounded text-white '>Login</button>
     <OAuth/>
 
    </form>
