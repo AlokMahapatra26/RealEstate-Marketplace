f60ddeece5e47cb02adef004eeb6f05bcf141232
AlokMahapatra26 <alokmahapatra2604@gmail.com>
2023-09-16 00:07:09 +0530

    made create listing page private

--- RealEstate-Marketplace/src/App.jsx
+++ RealEstate-Marketplace/src/App.jsx
@@ -24,7 +24,9 @@ function App() {
           <Route path='/profile' element={<Profile/>}/>
           </Route>
 
+          <Route path='/create-listing' element={<PrivateRoute/>}>
           <Route path='/create-listing' element={<CreateListing/>}/>
+          </Route>
           
           <Route path='/sign-in' element={<Signin/>}/>
 
